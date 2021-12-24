34bcc817383ae48b37f36b97e431e85124d0fd8b
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-24 18:44:28 +0530

    ADDED

---
+++ HarmonicNum.java/src/com/harmonicnum/HarmonicNum.java
@@ -0,0 +1,21 @@
+package com.harmonicnum;
+
+import java.util.Scanner;
+
+public class HarmonicNum {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the value of N:");
+		   Scanner sc = new Scanner(System.in);
+		   double N = sc.nextDouble();
+		    double HarmonNum = 0;
+		   for(double i = 1; i <= N; i++) {
+			    HarmonNum = HarmonNum+(1/i);
+		   }
+		    System.out.println("Nth Harmonic value is " +HarmonNum);
+		    sc.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
