4d9e4a8497921ec9cc35cb277721a8fbb35483e2
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-24 19:28:23 +0530

    ADDED

---
+++ TwoDArray.java/src/com/twodarray/TwoDArray.java
@@ -0,0 +1,54 @@
+package com.twodarray
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		System.out.println("Enter number of rows :");
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		System.out.println("Enter number of columns: ");
+		int n = sc.nextInt();
+		System.out.println("Rows : " + m + " columns : " + n);
+		
+		if (m > 0 && n > 0) {
+			TwoDArray twoDArray = new TwoDArray();
+			twoDArray.setArrayValues(m, n);
+		} else {
+			System.out.println("Invalid Input");
+		}
+	}
+	//to check the value of array
+	public void setArrayValues(int row, int col) {
+		int[][] twoDArr = new int [row][col];
+		PrintWriter sc;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				System.out.println("Enter value of [" + i + "][" + j + "] => ");
+				twoDArr[i][j] = ((Scanner) sc).nextInt();
+			}
+		}
+		sc.close();
+		TwoDArray twoDArray = new TwoDArray();
+		twoDArray.printArrayValues(twoDArr, row, col);
+	}
+	//to print 2D Array
+	public void printArrayValues(int[][] twoDArr, int row, int col) {
+		PrintWriter writer = new PrintWriter(System.out);
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				writer.write(""+twoDArr[i][j]+"\t");
+				writer.flush();
+			}
+			writer.write("\n");
+			writer.flush();
+		}
+		writer.close();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
