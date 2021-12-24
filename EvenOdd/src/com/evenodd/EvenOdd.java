7089cd7093002a16f375688fdf7265743ec48978
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-24 18:36:02 +0530

    ADDED

---
+++ EvenOdd/src/com/evenodd/EvenOdd.java
@@ -0,0 +1,22 @@
+package com.evenodd;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number of your choice :");
+	int N = sc.nextInt();
+	sc.close();
+	
+	if(N % 2 == 0) {
+		System.out.println("Number is even.");
+	}else {
+		System.out.println("Number is odd.");
+	}
+}
+		// TODO Auto-generated method stub
+
+	}
+
