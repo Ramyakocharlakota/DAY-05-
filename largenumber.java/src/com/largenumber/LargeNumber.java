dd78cf6fa45a3f129eeac318dfd6cbd789a568e6
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-24 18:47:51 +0530

    ADDED

---
+++ largenumber.java/src/com/largenumber/LargeNumber.java
@@ -0,0 +1,27 @@
+package com.largenumber;
+
+import java.util.Scanner;
+
+public class LargeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter the value of x :");
+	       int x = sc.nextInt();
+	      System.out.println("Enter the value of y :");
+	       int y = sc.nextInt();
+	      System.out.println("Enter the value of z :");
+	       int z = sc.nextInt();
+	       sc.close();
+	       if (x > y && x > z) {
+	 	      System.out.println("The largest number is :" +x);   
+	       }else if(y > x && y > z) {
+	          System.out.println("The largest number is :" +y);  
+	       }else {
+		          System.out.println("The largest number is :" +z);  
+	       }
+		// TODO Auto-generated method stub
+
+	}
+
+}
