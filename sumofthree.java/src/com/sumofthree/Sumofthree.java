dde0c746ca5f4f96fbc294d9b48e734eb00f2dd5
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-24 19:19:36 +0530

    ADDED

---
+++ sumofthree.java/src/com/sumofthree/Sumofthree.java
@@ -0,0 +1,19 @@
+package com.sumofthree;
+
+public class Sumofthree {
+
+	public static void main(String[] args) {
+		int a[] = new int[3];
+		a[0]=2;
+		a[1]=3;
+		a[2]=-5;
+		int sum = 0;
+		for(int i = 0; i < a.length ; i++) {
+			sum =  a[0] + a[1] + a[2];
+		}
+		System.out.println(sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
